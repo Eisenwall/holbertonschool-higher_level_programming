5b2af92a6ec5bc795347618e015f2681f1b7fd58
Eisenwall <Eisenwall@proton.me>
2025-09-06 14:26:57 +0400

    HTS

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax"
-
-print(str[39:66], str[-22:-18], str[:6])
+# DO NOT redefine str, just use it
+print(str[39:66] + " with " + str[:6])
