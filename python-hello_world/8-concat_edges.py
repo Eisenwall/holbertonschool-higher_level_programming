a15e84e1d212cc3954c929dfc0a0e81f9d1ed9f2
Eisenwall <Eisenwall@proton.me>
2025-09-06 13:48:40 +0400

    HTS

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(str[38:53] + str[53] + str[54:65] + str[75:81] + str[:6])
+print(str[38:53] + str[53] +
+      str[54:65] + str[75:81] +
+      str[:6])
