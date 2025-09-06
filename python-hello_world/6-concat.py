96a8adc12a1f94147abec6bf843e38877092206e
Eisenwall <Eisenwall@proton.me>
2025-09-06 13:34:39 +0400

    HTS

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str1 = "Welcome to "
-str2 = "Holberton School!"
-new_str = str1 + str2
-print(new_str)
+str1 = "Holberton"
+str2 = "School"
+str1 = "{} {}".format(str1, str2)
+print(f"Welcome to {str1}")
