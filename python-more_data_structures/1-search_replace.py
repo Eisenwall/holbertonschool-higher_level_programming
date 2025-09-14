88573480c89516ea1151db38fa3ee321a6822d7d
Eisenwall <Eisenwall@proton.me>
2025-09-14 19:39:30 +0000

    HTS

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     """
-    Returns a new list where all occurrences of 'search' are replaced by 'replace'.
+    Return a new list where all occurrences of 'search' are replaced
+    by 'replace'.
     """
     return [replace if x == search else x for x in my_list]
