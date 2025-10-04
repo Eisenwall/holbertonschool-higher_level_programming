1b13bb388d2db212f65fad4a3f3f417bb4406a4e
Eisenwall <Eisenwall@proton.me>
2025-10-04 18:02:34 +0000

    HTS

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -58,7 +58,7 @@ class Rectangle:
         return 2 * (self.__width + self.__height)
 
     def __str__(self):
-        
+        """Return the rectangle string representation using print_symbol."""
         if self.__width == 0 or self.__height == 0:
             return ""
         symbol = str(self.print_symbol)
@@ -66,8 +66,8 @@ class Rectangle:
         return "\n".join(lines)
 
     def __repr__(self):
-
-        return f"Rectangle({self.__width}, {self.__height})"
+        """Return a string representation to recreate a new instance using eval()."""
+        return "Rectangle({}, {})".format(self.__width, self.__height)
 
     def __del__(self):
         """Print a message when an instance is deleted and decrement the instance counter."""
