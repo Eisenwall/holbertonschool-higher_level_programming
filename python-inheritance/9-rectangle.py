3934912846432dc7285bb963324e011dcbbe9792
Eisenwall <Eisenwall@proton.me>
2025-10-04 19:09:04 +0000

    HTS

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Module that defines Rectangle class inheriting from BaseGeometry"""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle class inherits from BaseGeometry"""
+
+    def __init__(self, width, height):
+        """Initialize Rectangle with private width and height"""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """Return the area of the rectangle"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """Return string representation: [Rectangle] <width>/<height>"""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
