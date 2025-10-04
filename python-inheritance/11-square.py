29cd434e730e852934342993159072f566562b0f
Eisenwall <Eisenwall@proton.me>
2025-10-04 19:10:07 +0000

    HTS

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Module that defines Square class inheriting from Rectangle"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square class inherits from Rectangle"""
+
+    def __init__(self, size):
+        """Initialize Square with private size"""
+        self.integer_validator("size", size)
+        self.__size = size
+
+        super().__init__(size, size)
+
+    def __str__(self):
+        """Return string representation: [Square] <width>/<height>"""
+        return "[Square] {}/{}".format(self._Rectangle__width, self._Rectangle__height)
