c6747e04b6ed9f6cda2ef6d252beb7da60aedb89
Eisenwall <Eisenwall@proton.me>
2025-10-05 11:02:50 +0000

    HTS

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 """Module defining MyList class"""
 
-
 class MyList(list):
     """Custom list class that inherits from list"""
 
     def print_sorted(self):
-        """Prints the list in ascending order"""
-        print(sorted(self))
+        """Prints the list in ascending order without modifying it"""
+        try:
+            print(sorted(self))
+        except TypeError:
+            
+            print(sorted(self, key=lambda x: str(x)))
