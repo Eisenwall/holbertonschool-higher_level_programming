9f1459a05775320dda789ea654fd3c51e1cd3f48
Eisenwall <Eisenwall@proton.me>
2025-10-06 15:01:08 +0000

    HTS

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,19 +1,13 @@
 #!/usr/bin/python3
-"""Module that defines Square class inheriting from Rectangle"""
-
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """Square class inherits from Rectangle"""
-
     def __init__(self, size):
-        """Initialize Square with private size"""
-        self.integer_validator("size", size)
+        self.integer_validator(size, size)
+        super().__init__(size, size)
         self.__size = size
 
-        super().__init__(size, size)
-
     def __str__(self):
-        """Return string representation: [Square] <width>/<height>"""
-        return "[Square] {}/{}".format(self._Rectangle__width, self._Rectangle__height)
+        return '[Square] ' + str(self.__size) + '/' + str(self.__size)
