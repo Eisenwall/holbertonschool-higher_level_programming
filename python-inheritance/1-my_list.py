0dc779e936c58e952f5d19d80317e21dfb704a9b
Eisenwall <Eisenwall@proton.me>
2025-10-06 14:32:57 +0000

    HTS

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -7,5 +7,5 @@ class MyList(list):
 
     def print_sorted(self):
         """Method to print sorted list"""
-        new_list = sorted(self)
-        print(new_list)
+        if issubclass(MyList, list):
+        print(sorted(self))
