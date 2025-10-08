447fdd31516585ce1b5beccfd202b32b01834bbf
Eisenwall <Eisenwall@proton.me>
2025-10-08 18:15:28 +0000

    HTS

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -1,43 +1,26 @@
 #!/usr/bin/python3
-"""Defines a Student class with JSON serialization and deserialization"""
+"""Module that defines a Student class with serialization methods"""
+
 
 class Student:
     """Represents a student"""
 
     def __init__(self, first_name, last_name, age):
-        """
-        Initialize a Student instance.
-
-        Args:
-            first_name (str): First name of the student.
-            last_name (str): Last name of the student.
-            age (int): Age of the student.
-        """
+        """Initialize a Student instance"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """
-        Retrieve a dictionary representation of the Student instance.
+        """Return a dictionary representation of the Student
 
-        Args:
-            attrs (list, optional): List of attribute names to retrieve.
-                                    If None, return all attributes.
-
-        Returns:
-            dict: Dictionary of requested attributes.
+        If attrs is a list of strings, only include those attributes
         """
         if attrs is None:
             return self.__dict__.copy()
-        return {key: value for key, value in self.__dict__.items() if key in attrs}
+        return {k: v for k, v in self.__dict__.items() if k in attrs}
 
     def reload_from_json(self, json):
-        """
-        Replace all attributes of the Student instance with values from a dictionary.
-
-        Args:
-            json (dict): Dictionary containing attribute names and values.
-        """
+        """Replace all attributes of the Student instance"""
         for key, value in json.items():
             setattr(self, key, value)
