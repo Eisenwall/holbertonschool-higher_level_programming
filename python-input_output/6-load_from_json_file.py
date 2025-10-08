834a8b6dd315c6e82e16e957d6845cb618ce5919
Eisenwall <Eisenwall@proton.me>
2025-10-08 18:06:25 +0000

    HTS

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Function that creates an object from a JSON file"""
+
+import json
+
+def load_from_json_file(filename):
+    """
+    Creates a Python object from a JSON file.
+
+    Args:
+        filename (str): Path to the JSON file.
+
+    Returns:
+        object: Python object (dict, list, etc.) represented in the JSON file.
+    """
+    with open(filename, 'r', encoding='utf-8') as f:
+        return json.load(f)
