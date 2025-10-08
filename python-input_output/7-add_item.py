e224d26f2def8105aafd55ef8673f8fc069ee467
Eisenwall <Eisenwall@proton.me>
2025-10-08 18:10:25 +0000

    HTS

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -3,8 +3,10 @@
 
 import sys
 from pathlib import Path
+
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
 
 filename = "add_item.json"
 
