27baa50093346a755f6873d8ede0001cc429d76c
Eisenwall <Eisenwall@proton.me>
2025-11-01 19:32:14 +0000

    HTS

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,8 @@
--- 0-privileges.sql
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+SELECT id, name
+FROM cities
+WHERE state_id = (
+    SELECT id
+    FROM states
+    WHERE name = 'California'
+)
+ORDER BY id ASC;
