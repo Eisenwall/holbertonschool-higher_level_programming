71b4dd52818499aaea9f624d557e6e76a68b5619
Eisenwall <Eisenwall@proton.me>
2025-11-01 19:30:19 +0000

    HTS

---
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,6 @@
+SELECT genres.name AS genre, COUNT(tv_show_genres.tv_show_id) AS number_of_shows
+FROM tv_show_genres
+JOIN genres ON tv_show_genres.genre_id = genres.id
+GROUP BY genres.name
+HAVING number_of_shows > 0
+ORDER BY number_of_shows DESC;
