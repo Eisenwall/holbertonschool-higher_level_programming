48e89ae893fedace1b2e2af53cd2c7196007a4f1
Eisenwall <Eisenwall@proton.me>
2025-11-02 08:02:01 +0000

    HTS

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """Print all City objects from the database hbtn_0e_14_usa"""
+
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 from model_city import City  # Import City so SQLAlchemy knows about it
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
@@ -14,7 +16,9 @@ if __name__ == "__main__":
     username, password, db_name = sys.argv[1], sys.argv[2], sys.argv[3]
 
     # Connect to MySQL database
-    engine = create_engine(f"mysql+mysqldb://{username}:{password}@localhost/{db_name}")
+    engine = create_engine(
+        f"mysql+mysqldb://{username}:{password}@localhost/{db_name}"
+    )
     Base.metadata.create_all(engine)
 
     # Create session
@@ -26,5 +30,5 @@ if __name__ == "__main__":
 
     for city in cities:
         print(f"{city.state.name}: ({city.id}) {city.name}")
-    
+
     session.close()
--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """City class definition linked to MySQL table 'cities'"""
+
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 from model_state import Base  # Base must be imported from model_state
+
 
 class City(Base):
     """Class representing a city"""
@@ -11,6 +13,6 @@ class City(Base):
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
-    
+
     # Relationship to State to allow access to state.name
     state = relationship("State", backref="cities")
