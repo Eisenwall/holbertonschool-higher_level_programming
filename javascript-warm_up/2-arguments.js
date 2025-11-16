bc2f0f60336a2c6a9371c74a52411bbf78e14464
Eisenwall <Eisenwall@proton.me>
2025-11-16 15:32:15 +0000

    HTS

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const args = process.argv.slice(2); // get all arguments after the script name
+
+if (args.length === 0) {
+  console.log('No argument');
+} else if (args.length === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
